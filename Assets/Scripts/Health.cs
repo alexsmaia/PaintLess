19a052d6e29ccc6c4eed0e42829abf19e3298df6
Alexandre Maia <alex.maia.vc@gmail.com>
2020-01-25 00:08:44 +0000

    Alterações nivel1

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,17 +6,12 @@ public class Health : MonoBehaviour
 
     // Set Variables
     public Rigidbody player;
-    public Text healthTxt;
-
 
     void FixedUpdate()
     {
 
         // Lose health while moving
         Singletons.instance.pHealth -= player.velocity.magnitude * 0.01f;
-
-        // Change Health text value
-        healthTxt.text = Singletons.instance.pHealth.ToString();
 
         if(Singletons.instance.pHealth <= 0)
         {
